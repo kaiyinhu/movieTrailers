e5300328b2b6e8a58bddda0b0ee5ec04babab420
Kaiyin Hu <hukaiyin@gmail.com>
2016-12-25 20:58:57 -0800

    Add files via upload

---
+++ media.py
@@ -0,0 +1,9 @@
+import webbrowser
+class Movie():
+	def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+		self.title = movie_title
+	        self.poster_image_url = poster_image_url
+	        self.trailer_youtube_url = trailer_youtube_url
+
+        def playTrailer(self):
+                webbrowser.open(self.trailer_youtube_url)
